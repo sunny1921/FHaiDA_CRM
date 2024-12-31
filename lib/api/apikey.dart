b5bfb7aad56344d925f10c56348f1751b723f2af
sunny1921 <21cs3066@rgipt.ac.in>
2024-12-31 15:55:19 +0530

    Remove API key and add template

--- lib/api/apikey.dart
+++ lib/api/apikey.dart
@@ -1,3 +1,4 @@
-const api_key = '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';
+// Instead of hardcoding the API key, use environment variables
+const String OPENAI_API_KEY = const String.fromEnvironment('OPENAI_API_KEY');
 const mapbox_key =
     'pk.eyJ1Ijoic2FtbWluZ2FzMSIsImEiOiJjbHNicHpjbGEwYnpmMnZsNWRsbzZ1b25qIn0.q03Rgt2P9GjJb6utXNU8LQ';
